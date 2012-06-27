71a7131419485f074e8aba58052f32508fb42ab9
Brandon Callender <bmcallender@uchicago.edu>
2012-06-27 15:02:39 -0700

    Version 1.0

--- QuickQuiz/ViewController.h
+++ QuickQuiz/ViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+
+}
+@property (weak, nonatomic) IBOutlet UIWebView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UIButton *Left;
+@property (weak, nonatomic) IBOutlet UIButton *Right;
+
+@property (strong, nonatomic) NSString *Name;
+
 
 @end
--- QuickQuiz/ViewController.m
+++ QuickQuiz/ViewController.m
@@ -13,15 +13,86 @@
 @end
 
 @implementation ViewController
+@synthesize imageView;
+@synthesize resultLabel;
+@synthesize Left;
+@synthesize Right;
+@synthesize Name;
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self loadPicture];
+  
+    
 }
+- (void)loadPicture
+{
+    NSDictionary *mainDict = [[NSDictionary alloc] initWithObjectsAndKeys:
+                              @"http://blogs.honeywellsecurity.com/content/uploads/2011/11/John-Smith1.jpg", @"Johnny Appleseed",
+                              @"http://cnhsg.net/images659/Meg%20Brown.jpg", @"Meg Brown",
+                              @"http://api.ning.com/files/*p1hq6Wg1OkS*NhHSvy*0q1MyaQhLISN1bfx0R8nYDS68QZkjCLixr1iRndthxmSSVn6fFiS-iUyTTQjzPwOK3Y3jrc31Qt0FdbAJmG49gA_/matt_dallas_kyle_xy.jpg", @"Kyle Chron",
+                              @"http://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Jack_Layton_-_2011.jpg/220px-Jack_Layton_-_2011.jpg", @"Jack Davies",
+                              @"http://static.dailystrength.org/groupfiles/1/6/2/1/10001261/g_1944756615.jpg", @"Lily Evans",
+                              @"http://ia.media-imdb.com/images/M/MV5BNDg3Nzk5NTQyN15BMl5BanBnXkFtZTcwNTA4ODI2Mw@@._V1._SX640_SY360_.jpg", @"Dora Finkelstein",@"https://pictures.dealer.com/a/airportcdjcllc/1289/0b853c144046387200489c420a31a180.jpg", @"Eric Graves",@"http://taijiacupuncture.com/images/jane-hilton-headshot.jpg", @"Jane Hilton",nil];
+    NSLog(@"Test");
+    int r = arc4random() % [mainDict count];
+    NSString *currentName = [[NSString alloc] initWithString:[[mainDict allKeys] objectAtIndex:r]];
+    NSString *currentURL = [[NSString alloc] initWithString:[mainDict objectForKey:currentName]];
+    NSURL *url = [[NSURL alloc] initWithString:currentURL];
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:url];
+    [imageView loadRequest:req];
+    self.Name = currentName;
+    int ri = arc4random() % [mainDict count];
+    int rans = arc4random() % 6;
+    if(rans <= 2){ 
+    [Left setTitle:currentName forState:UIControlStateNormal];
+        if ([[[mainDict allKeys] objectAtIndex:ri] isEqualToString:currentName]) {
+            [Right setTitle:[[NSString alloc] initWithString:[[mainDict allKeys] objectAtIndex:ri-1]] forState:UIControlStateNormal];
+        }else{
+            [Right setTitle:[[NSString alloc] initWithString:[[mainDict allKeys] objectAtIndex:ri]] forState:UIControlStateNormal];}}
+    else {
+        [Right setTitle:currentName forState:UIControlStateNormal];
+        if ([[[mainDict allKeys] objectAtIndex:ri] isEqualToString:currentName]) {
+            [Left setTitle:[[NSString alloc] initWithString:[[mainDict allKeys] objectAtIndex:ri-1]] forState:UIControlStateNormal];
+        }else{
+            [Left setTitle:[[NSString alloc] initWithString:[[mainDict allKeys] objectAtIndex:ri]] forState:UIControlStateNormal];}
+    }
+}
+- (IBAction)LeftClick:(id)sender {
+    if ([Left.titleLabel.text isEqualToString:self.Name]) {
+        resultLabel.text = @"Success!";
+        [self loadPicture];
+    }
+    else {
+        resultLabel.text = @"Failure...";
+        [self loadPicture];
+    }
+}
+- (IBAction)RightClick:(id)sender {
+    if ([Right.titleLabel.text isEqualToString:self.Name]) {
+        resultLabel.text = @"Success!";
+        [self loadPicture];
+    }
+    else {
+        resultLabel.text = @"Failure...";
+        [self loadPicture];
+    }
+
+}
+
+
+
 
 - (void)viewDidUnload
 {
+
+    [self setImageView:nil];
+    [self setResultLabel:nil];
+    [self setLeft:nil];
+    [self setRight:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
